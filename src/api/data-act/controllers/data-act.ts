1a984c7d29f5b15595de9776717db335844dc18b
Amine Barradouane <barradouanea@gmail.com>
2025-07-03 18:52:35 +0200

    1er commit

---
+++ src/api/data-act/controllers/data-act.ts
@@ -0,0 +1,7 @@
+/**
+ * data-act controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::data-act.data-act');
---
+++ src/api/data-act/routes/data-act.ts
@@ -0,0 +1,7 @@
+/**
+ * data-act router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::data-act.data-act');
